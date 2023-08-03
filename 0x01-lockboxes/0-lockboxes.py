0f130f8c004f63e8acccf98eb15e636b9aa4a009
Michael Erameh <eramehmichael2000@gmail.com>
2023-08-03 13:45:41 +0100

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,14 +6,14 @@
 def canUnlockAll(boxes):
     '''checks boxes in a list
     '''
-    va = len(boxes)
+    n = len(boxes)
     seenBoxes = set([0])
     unseenBoxes = set(boxes[0]).difference(set([0]))
     while len(unseenBoxes) > 0:
         boxId = unseenBoxes.pop()
-        if not boxId or boxIdx >= va or boxId < 0:
+        if not boxId or boxIdx >= n or boxId < 0:
             continue
         if boxId not in seenBoxes:
             unseenBoxes = unseenBoxes.union(boxes[boxId])
             seenBoxes.add(boxId)
-    return va == len(seenBoxes)
+    return n == len(seenBoxes)
