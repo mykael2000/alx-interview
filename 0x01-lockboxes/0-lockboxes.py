651ae5ff802a063314800a8982a13287714af8d3
Michael Erameh <eramehmichael2000@gmail.com>
2023-08-03 13:41:20 +0100

    done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' working with lockboxes.
+'''
+
+
+def canUnlockAll(boxes):
+    va = len(boxes)
+    seenBoxes = set([0])
+    unseenBoxes = set(boxes[0]).difference(set([0]))
+    while len(unseenBoxes) > 0:
+        boxId = unseenBoxes.pop()
+        if not boxId or boxIdx >= va or boxId < 0:
+            continue
+        if boxId not in seenBoxes:
+            unseenBoxes = unseenBoxes.union(boxes[boxId])
+            seenBoxes.add(boxId)
+    return va == len(seenBoxes)
